ec987e58a61bdf9f5864413247ffe8307a09a380
Vasyl Kuzurman <fasd123435@gmail.com>
2021-08-16 17:43:42 +0200

    src/Main.java Added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int price = 5_000;
+        int mile = 20;
+        int totalBonus = price / mile;
+        System.out.println("Ваши бонусные мили: " + totalBonus);
+    }
 }
